af2ab6370117af503054dac249fefd74f63ad573
Dmitry Maretov <maretov@gmail.com>
2025-09-15 21:56:55 +0300

    ADD route settings (React Router)

---
+++ frontend/src/Pages.jsx
@@ -0,0 +1,23 @@
+const MainPage = () => (
+	<div>
+		MainPage
+	</div>
+)
+
+const LoginPage = () => (
+	<div>
+		LoginPage
+	</div>
+)
+
+const ErrorPage = () => (
+	<h1>
+		Такой страницы не существует
+	</h1>
+)
+
+export {
+	MainPage,
+	LoginPage,
+	ErrorPage,
+}
