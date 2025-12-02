85a837d3aca9a408c7b2329effedf2121e4486a9
Dmitry Maretov <maretov@gmail.com>
2025-12-02 20:51:46 +0300

    REFACTOR code

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -8,37 +8,173 @@ import axios from 'axios'
 import path from '../routes.js'
 import { js, normalize, filterMessages, renderMessages } from '../utils.jsx'
 
-const MainPage = () => {
-	const navigate = useNavigate()
-	const location = useLocation()
+
+// HEADER AREA
+const NavBar = () => {
 	const dispatch = useDispatch()
-	const { token } = useSelector(state => state.auth)
+
+	const logout = () => {
+		localStorage.removeItem('authToken')
+		dispatch(removeCredentials())
+	}
+
+	return (
+		<nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+			<div className="container">
+				<a className="navbar-brand" href="/">Hexlet Chat</a>
+				<button onClick={logout} type="button" className="btn btn-primary">Выйти</button>
+			</div>
+		</nav>
+	)
+}
+
+
+// CHANNELS AREA
+const ChannelsArea = () => {
 	const { channels, activeChannel } = useSelector(state => state.channels)
+	const dispatch = useDispatch()
+
+	const Header = () => (
+		<div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+				<b>Каналы</b>
+				<button type="button" className="p-0 text-primary btn btn-group-vertical">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-plus-square">
+						<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path>
+						<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
+					</svg>
+					<span className="visually-hidden">+</span>
+				</button>
+			</div>
+	)
+
+	const Channels = () => (
+		<ul id="channel-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+			{Object.values(channels).map((channel) => {
+				const { id, name } = channel
+				let buttonClass = 'w-100 rounded-0 text-start btn'
+				if (id === activeChannel.id) {
+					buttonClass += ' btn-secondary'
+				}
+				return (
+					<li key={id} className="nav-item w-100">
+						<button
+							onClick={() => dispatch(setActiveChannel(channel))}
+							type="button"
+							className={buttonClass}
+						>
+							<span className="me-1">#</span>
+							{name}
+						</button>
+					</li>
+				)
+			})}
+		</ul>
+	)
+
+	return (
+		<div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+			<Header />
+			<Channels />
+		</div>
+	)
+}
+
+
+// CHAT AREA
+const ChatArea = () => {
+	const { activeChannel } = useSelector(state => state.channels)
 	const { messages } = useSelector(state => state.messages)
-	const authToken = localStorage.getItem('authToken')
-	
+
 	const inputRef = useRef()
 	const [newMessage, setNewMessage] = useState('')
+	
 	const handleChangeNewMessage = ({ target }) => {
 		setNewMessage(target.value)
-		// console.log(inputRef.current)
-		// setTimeout(() => inputRef.current.focus(), 0)
-		// inputRef.current.focus()
-		// console.log(inputRef.current)
+		inputRef.current.focus()
 	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		console.log(newMessage)
-		inputRef.current.value = ''
+		setNewMessage('')
+		// 1. Отправить новое сообщение POST запросом.
+		// 		Тело запроса { body: 'new message', channelId: '1', usename: 'user' }.
+		//		Добавить к телу заголовки
+		// 2. Получить подтверждение через websockets
+		// 		В теле ответа будет возвращено новое сообщение с добавленным ID
+		// 3. Сохранить сообщение в state.
+		//  	Произойдет перерисовка чата
 	}
 
-	// деавторизуем пользователя
-	const logout = () => {
-		localStorage.removeItem('authToken')
-		dispatch(removeCredentials())
-	}
+	useEffect(() => {
+		inputRef.current.focus()
+	})
+
+	const activeMessages = (activeChannel ? filterMessages(messages, activeChannel.id) : {})
+	const messagesCount = Object.keys(activeMessages).length
+
+	const Header = () => (
+		<div className="bg-light mb-4 p-3 shadow-sm small">
+			<p className="m-0">
+				<b>
+					<span># </span>
+					{activeChannel && activeChannel.name}
+				</b>
+			</p>
+			<span className="text-muted">{`${messagesCount} сообщений - ИСПРАВИТЬ склонения (i18next)!`}</span>
+		</div>
+	)
+
+	const Messages = () => (
+		<div id="messages-box" className="chat-messages overflow-auto px-5">
+			{renderMessages(activeMessages)}
+		</div>
+	)
+
+	const Input = () => (
+		<div className="mt-auto px-5 py-3">
+			<form onSubmit={handleSubmit} noValidate={true} className="py-1 border rounded-2">
+				<div className="input-group">
+					<input
+						ref={inputRef}
+						onChange={handleChangeNewMessage}
+						value={newMessage}
+						name="body"
+						aria-label="Новое сообщение"
+						placeholder="Введите сообщение..."
+						className="border-0 p-0 ps-2 form-control"
+					/>
+					<button type="submit" className="btn btn-group-vertical" disabled={!newMessage}>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square">
+							<path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
+						</svg>
+						<span className="visually-hidden">Отправить</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	)
+
+	return (
+		<div className="col p-0 h-100">
+			<div className="d-flex flex-column h-100">
+				<Header />
+				<Messages />
+				<Input />
+			</div>
+		</div>
+	)
+}
+
+
+// MAIN PAGE
+const MainPage = () => {
+	const navigate = useNavigate()
+	const location = useLocation()
+	const dispatch = useDispatch()
+	const { token } = useSelector(state => state.auth)
 	
+	const authToken = localStorage.getItem('authToken')
 
 	// если есть токен в localStorage, то добавляем его в состояние
 	// если токена в localStorage нет, то перенаправляем на страницу входа
@@ -88,129 +224,13 @@ const MainPage = () => {
 		fetchData()
 	}, [token])
 
-	useEffect(() => {
-		inputRef.current.focus()
-	})
-
-	const NavBar = () => (
-		<nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-			<div className="container">
-				<a className="navbar-brand" href="/">Hexlet Chat</a>
-				<button onClick={logout} type="button" className="btn btn-primary">Выйти</button>
-			</div>
-		</nav>
-	)
-
-	const ChannelsArea = () => {
-		const Header = () => (
-			<div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-					<b>Каналы</b>
-					<button type="button" className="p-0 text-primary btn btn-group-vertical">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-plus-square">
-							<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path>
-							<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
-						</svg>
-						<span className="visually-hidden">+</span>
-					</button>
-				</div>
-		)
-
-		const Channels = () => (
-			<ul id="channel-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-				{Object.values(channels).map((channel) => {
-					const { id, name } = channel
-					let buttonClass = 'w-100 rounded-0 text-start btn'
-					if (id === activeChannel.id) {
-						buttonClass += ' btn-secondary'
-					}
-					return (
-						<li key={id} className="nav-item w-100">
-							<button
-								onClick={() => dispatch(setActiveChannel(channel))}
-								type="button"
-								className={buttonClass}
-							>
-								<span className="me-1">#</span>
-								{name}
-							</button>
-						</li>
-					)
-				})}
-			</ul>
-		)
-
-		return (
-			<div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-				<Header />
-				<Channels />
-			</div>
-		)
-	}
-
-	const MessagesArea = () => {
-		const activeMessages = (activeChannel ? filterMessages(messages, activeChannel.id) : {})
-		const messagesCount = Object.keys(activeMessages).length
-
-		const Header = () => (
-			<div className="bg-light mb-4 p-3 shadow-sm small">
-				<p className="m-0">
-					<b>
-						<span># </span>
-						{activeChannel && activeChannel.name}
-					</b>
-				</p>
-				<span className="text-muted">{`${messagesCount} сообщений - ИСПРАВИТЬ склонения (i18next)!`}</span>
-			</div>
-		)
-
-		const Messages = () => (
-			<div id="messages-box" className="chat-messages overflow-auto px-5">
-				{renderMessages(activeMessages)}
-			</div>
-		)
-
-		const Input = () => (
-			<div className="mt-auto px-5 py-3">
-				<form onSubmit={handleSubmit} noValidate={true} className="py-1 border rounded-2">
-					<div className="input-group">
-						<input
-							ref={inputRef}
-							onChange={handleChangeNewMessage}
-							value={newMessage}
-							name="body"
-							aria-label="Новое сообщение"
-							placeholder="Введите сообщение..."
-							className="border-0 p-0 ps-2 form-control"
-						/>
-						<button type="submit" className="btn btn-group-vertical" disabled={!newMessage}>
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square">
-								<path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
-							</svg>
-							<span className="visually-hidden">Отправить</span>
-						</button>
-					</div>
-				</form>
-			</div>
-		)
-
-		return (
-			<div className="col p-0 h-100">
-				<div className="d-flex flex-column h-100">
-					<Header />
-					<Messages />
-					<Input />
-				</div>
-			</div>
-		)
-	}
-
 	return (
 		<div className="d-flex flex-column h-100">
 			<NavBar />
 			<div className="container h-100 my-4 overflow-hidden rounded shadow">
 				<div className="row h-100 bg-white flex-md-row">
 					<ChannelsArea />
-					<MessagesArea />
+					<ChatArea />
 				</div>
 			</div>
 		</div>
