a64bff03b940031e5b1b53caf8a5657c0bdedeea
Dmitry Maretov <maretov@gmail.com>
2026-01-08 22:54:20 +0300

    REFACTOR modal 'ChannelAdd' from bootstrap to react-bootstrap

--- frontend/src/pages/Modals.jsx
+++ frontend/src/pages/Modals.jsx
@@ -1,5 +1,6 @@
-import { Formik, Form, Field } from "formik"
+import { Formik } from "formik"
 import { useEffect, useRef } from "react"
+import { Modal, Form, Button } from "react-bootstrap"
 import * as yup from "yup"
 
 export const ChannelAdd = (props) => {
@@ -14,63 +15,59 @@ export const ChannelAdd = (props) => {
   }, [])
 
   return (
-    <Formik
-      initialValues={{ newChannel: ""}}
-      onSubmit={async (values) => {
-        onChannelAdd(values.newChannel)
-        onHide()
-      }}
-      validationSchema={
-        yup.object().shape({
-          newChannel: yup
-            .string()
-            .required("Обязательное поле")
-            .min(3, "От 3 до 20 символов")
-            .max(20, "От 3 до 20 символов")
-            .notOneOf(channelsNames, "Должно быть уникальным")
-        })
-      }
-    >
-      {({ errors, touched }) => {
-        const inputClasses = errors.newChannel && touched.newChannel ? "form-control is-invalid" : "form-control"
+    <Modal show={true} centered>
+      <Modal.Header closeButton onClick={onHide}>
+        <Modal.Title>Добавить канал</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
 
-        return (
-          <>
-            <div className="fade modal-backdrop show"></div>
-            <div aria-modal="true" className="fade modal show" tabIndex="-1" style={{ display: "block" }}>
-              <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <div className="modal-title h4">Добавить канал</div>
-                    <button
-                      onClick={onHide}
-                      type="button"
-                      aria-label="Close"
-                      data-bs-dismiss="modal"
-                      className="btn btn-close"
-                    >
-                    </button>
-                  </div>
-                  <div className="modal-body">
-                    <Form>
-                      <div className="form-group mb-2">
-                        <label htmlFor="newChannel" className="visually-hidden"></label>
-                        <Field ref={inputRef} type="text" name="newChannel" id="newChannel" className={inputClasses}></Field>
-                        {errors.newChannel && touched.newChannel ? <div className="invalid-feedback">{errors.newChannel}</div> : null}
-                      </div>
-                      <div className="d-flex justify-content-end">
-                        <button onClick={onHide} type="button" className="me-2 btn btn-secondary">Отменить</button>
-                        <button type="submit" className="btn btn-primary">Отправить</button>
-                      </div>
-                    </Form>
-                  </div>
+        <Formik
+          initialValues={{ newChannel: ""}}
+          onSubmit={async (values) => {
+            onChannelAdd(values.newChannel)
+            onHide()
+          }}
+          validationSchema={
+            yup.object().shape({
+              newChannel: yup
+                .string()
+                .required("Обязательное поле")
+                .min(3, "От 3 до 20 символов")
+                .max(20, "От 3 до 20 символов")
+                .notOneOf(channelsNames, "Должно быть уникальным")
+            })
+          }
+        >
+          {({ handleSubmit, handleChange, values, touched, errors }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label htmlFor="newChannel" className="visually-hidden">Имя канала</Form.Label>
+                
+                <Form.Control
+                  value={values.newChannel}
+                  onChange={handleChange}
+                  id="newChannel"
+                  name="newChannel"
+                  type="text"
+                  className="mb-2"
+                  isValid={!errors.newChannel && touched.newChannel}
+                  isInvalid={errors.newChannel && touched.newChannel}
+                  ref={inputRef}
+                >
+                </Form.Control>
+                
+                <Form.Control.Feedback type="invalid">{errors.newChannel}</Form.Control.Feedback>
+                
+                <div className="d-flex justify-content-end">
+                  <Button type="button" variant="secondary" className="me-2" onClick={onHide}>Отменить</Button>
+                  <Button type="submit">Отправить</Button>
                 </div>
-              </div>
-            </div>
-          </>
-        )
-      }}
-      
-    </Formik>
+                
+              </Form.Group>
+            </Form>
+          )}
+        </Formik>
+      </Modal.Body>
+    </Modal>
   )
 }
