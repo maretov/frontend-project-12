028f0beaff0049bc19050337d0d7d015de3eb1dc
Dmitry Maretov <maretov@gmail.com>
2026-01-31 20:30:30 +0300

    REFACTOR ErrorPage.jsx

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -1,7 +1,21 @@
+import Header from "./Header"
+import { Link } from "react-router"
+
 const ErrorPage = () => (
-	<h1>
-		Такой страницы не существует
-	</h1>
+	<div className="d-flex flex-column h-100">
+		<Header />
+		<div className="text-center">
+			<img
+				src="./src/assets/avatar-error.svg"
+				alt="Страница не найдена"
+				className="img-fluid h-25"
+			/>
+			<h1 className="h4 text-muted">Страница не найдена</h1>
+			<p>Но вы можете перейти
+			<Link to="/"> на главную страницу</Link>
+			</p>
+		</div>
+	</div>
 )
 
 export default ErrorPage
